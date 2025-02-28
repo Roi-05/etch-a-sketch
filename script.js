455b6f28b349baf964d6b0dee8b31a5e8628be12
John Roi <03jrd05@gmail.com>
2025-02-28 16:47:33 +0800

    Add color to grids when hovered

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const gridRatio = 16;
 
     for (let i = 0; i < gridRatio * gridRatio; i++) {
         const grids = document.createElement("div");
-        grids.classList.toggle("grid");
+        grids.classList.toggle("grids");
         container.appendChild(grids);
     }
 
 const grids = document.querySelectorAll(".grids");
 
 grids.forEach((grid) => {
-    grid.addEventListener("click", () => {
-        console.log("Not working idiot!")
-        grid.style.backgroundColor = "black";
+    grid.addEventListener("mouseover", () => {
+        grid.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     });
 });
