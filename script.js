6636c0e660df9e2a8bf86d6bca1c233a7891124c
John Roi <03jrd05@gmail.com>
2025-02-28 17:40:03 +0800

    Add an event listener to the button to prompt the user their desired grid row/column

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const container = document.querySelector("#container");
-const gridRatio = 16;
+const containerSize = 600;
 
-    for (let i = 0; i < gridRatio * gridRatio; i++) {
+const button = document.querySelector("#getWH");
+
+let height = 16;
+let width = 16;
+
+createGrids(height, width);
+
+button.setAttribute("style" , "background-color: #4681f4; padding: 10px 20px; border-radius: 8px; border: none; color: white; cursor: pointer;")
+
+button.addEventListener("click" , () => {
+     height = prompt("Enter row (1-100): ");
+     width = prompt("Enter column (1-100): ");
+     
+     if (height > 100 || height < 1 || width > 100 || width < 1) {
+        alert("Please enter between 1-100");
+        return;
+     }
+     createGrids(height, width);
+});
+
+function createGrids (height, width) {
+    container.innerHTML = "";
+    for (let i = 0; i < height * width; i++) {
         const grids = document.createElement("div");
+        grids.style.height = `${containerSize / height}px`;
+        grids.style.width = `${containerSize / width}px`;
         grids.classList.toggle("grids");
         container.appendChild(grids);
+
+        grids.addEventListener("mouseover", () => {
+            grids.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        });
     }
+}
 
-const grids = document.querySelectorAll(".grids");
-
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = `rgb( ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    });
-});
